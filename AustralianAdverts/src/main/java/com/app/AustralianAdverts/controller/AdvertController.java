abed04aee39b1d003d61e3e393412aca334af285
Korosive <eddoman33433@gmail.com>
2020-11-08 07:36:37 +1100

    Added adverts controller for API and miner alterations to advert service

---
+++ AustralianAdverts/src/main/java/com/app/AustralianAdverts/controller/AdvertController.java
@@ -0,0 +1,53 @@
+package com.app.AustralianAdverts.controller;
+
+import com.app.AustralianAdverts.model.Advert;
+import com.app.AustralianAdverts.service.AdvertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/adverts")
+public class AdvertController {
+
+    @Autowired
+    private AdvertService advertService;
+
+    @GetMapping(path = "/get/{advert_id}", produces = "application/json")
+    public HashMap<String, Object> getAdvertById(@PathVariable UUID advert_id) {
+        return advertService.getAdvertById(advert_id);
+    }
+
+    @GetMapping(path = "/get/user/{user_id}", produces = "application/json")
+    public HashMap<String, Object> getUserAdverts(@PathVariable UUID user_id) {
+        return advertService.getUserAdverts(user_id);
+    }
+
+    @GetMapping(path = "/get/live", produces = "application/json")
+    public List<Advert> getLiveAdverts() {
+        return advertService.getLiveAdverts();
+    }
+
+    @PutMapping(path = "/update-advert", consumes = "application/json", produces = "application/json")
+    public HashMap<String, Object> updateAdvert(@RequestBody Advert updateAdvert) {
+        return advertService.updateAdvert(updateAdvert);
+    }
+
+    @DeleteMapping(path = "/delete/{advert_id}", produces = "application/json")
+    public HashMap<String, Object> deleteAdvert(@PathVariable UUID advert_id) {
+        return advertService.deleteAdvertById(advert_id);
+    }
+
+    @PutMapping(path = "/update-paid/{advert_id}", produces = "application/json")
+    public HashMap<String, Object> updatePaid(@PathVariable UUID advert_id) {
+        return advertService.updatePaid(advert_id);
+    }
+
+    @PostMapping(path = "/create-advert", consumes = "application/json", produces = "application/json")
+    public HashMap<String, Object> createAdvert(@RequestBody Advert newAdvert) {
+        return advertService.createAdvert(newAdvert);
+    }
+}
