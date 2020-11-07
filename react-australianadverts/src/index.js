ad15944e48ef67e39c71c5692d72a362747cde26
Korosive <eddoman33433@gmail.com>
2020-11-08 08:46:57 +1100

    Base files and extra depenedencies (bootstrap and react-cookies)

--- react-australianadverts/src/index.js
+++ react-australianadverts/src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Login from './components/Login';
+import CreateUser from './components/CreateUser';
+import UserProfile from './components/UserProfile';
+import DeleteAdvert from './components/DeleteAdvert';
+import CreateAdvert from './components/CreateAdvert';
+import UpdateUser from './components/UpdateUser';
+import ProfileAdverts from './components/ProfileAdverts';
+import Advert from './components/Advert';
+import UpdateAdvert from './components/UpdateAdvert';
+import DeleteUser from './components/DeleteUser';
+import PayAdvert from './components/PayAdvert';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+	<BrowserRouter>
+		<Navigation />
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/login" component={Login} />
+			<Route path="/create/user" component={CreateUser} />
+			<Route path="/profile/:user_id" component={UserProfile} />
+			<Route path="/delete/advert/:advert_id" component={DeleteAdvert} />
+			<Route path="/delete/user/:user_id" component={DeleteUser} />
+			<Route path="/create/advert" component={CreateAdvert} />
+			<Route path="/profile/adverts/:user_id" component={ProfileAdverts} />
+			<Route path="/adverts/:advert_id" component={Advert}/>
+			<Route path="/update/advert/:advert_id" component={UpdateAdvert} />
+			<Route path="/update/user/:user_id" component{UpdateUser} />
+			<Route path="/pay/:advert_id" component={PayAdvert} />
+		</Switch>
+	</BrowserRouter>
 );
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
