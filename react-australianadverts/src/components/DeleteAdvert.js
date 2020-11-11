55804ad4e1079cc13d898d38424b36fbe8d313a0
Korosive <eddoman33433@gmail.com>
2020-11-11 18:26:49 +1100

    Created delete used and cleaned delete advert

--- react-australianadverts/src/components/DeleteAdvert.js
+++ react-australianadverts/src/components/DeleteAdvert.js
@@ -1,5 +1,4 @@
-import React, {Component, Fragment} from 'react';
-import {Redirect} from 'react-router-dom';
+import React from 'react';
 import axios from 'axios';
 
 export const DeleteAdvert = (props) => {
